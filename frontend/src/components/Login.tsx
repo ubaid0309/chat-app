663fab6d3ad5ab006746a26bc74449cf2865757c
ubaid0309 <ubaidskms@gmail.com>
2024-01-21 17:54:09 +0530

    add : form

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -17,7 +17,9 @@ const Login = () => {
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
-  function submitHandler() {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  function submitHandler(e: any) {
+    e.preventDefault();
     console.log(email, password);
   }
   return (
@@ -32,174 +34,191 @@ const Login = () => {
         </div>
         <div className="right-side w-full md:w-[50%]">
           {activeForm === "sign-in" ? (
-            <Card className="flex flex-col gap-2 ">
-              <CardHeader>
-                <CardTitle className="font-poppins text-3xl font-semibold">
-                  Sign In
-                  <p className="font-normal text-sm mt-2">
-                    New to Chat App ?{" "}
-                    <button
-                      className="text-blue-700 font-medium"
-                      onClick={() => setActiveForm("sign-up")}
-                    >
-                      Create an account
-                    </button>
-                  </p>
-                </CardTitle>
-              </CardHeader>
-              <CardContent className="flex flex-col gap-4">
-                <div>
-                  <p className="flex gap-1 text-sm font-medium px-1 mb-1">
-                    Email <span className="text-red-500">*</span>
-                  </p>
-                  <div className="flex items-center border px-2 rounded-md">
-                    <Input
-                      type="email"
-                      placeholder="Enter your email"
-                      className="placeholder:font-poppins"
-                      value={email}
-                      onChange={(e) => setEmail(e.target.value)}
-                    />
+            <form>
+              <Card className="flex flex-col gap-2 ">
+                <CardHeader>
+                  <CardTitle className="font-poppins text-3xl font-semibold">
+                    Sign In
+                    <p className="font-normal text-sm mt-2">
+                      New to Chat App ?{" "}
+                      <button
+                        className="text-blue-700 font-medium"
+                        onClick={() => setActiveForm("sign-up")}
+                      >
+                        Create an account
+                      </button>
+                    </p>
+                  </CardTitle>
+                </CardHeader>
+                <CardContent className="flex flex-col gap-4">
+                  <div>
+                    <p className="flex gap-1 text-sm font-medium px-1 mb-1">
+                      Email <span className="text-red-500">*</span>
+                    </p>
+                    <div className="flex items-center border px-2 rounded-md">
+                      <Input
+                        required
+                        type="email"
+                        placeholder="Enter your email"
+                        className="placeholder:font-poppins"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                      />
+                    </div>
                   </div>
-                </div>
 
-                <div>
-                  <p className="flex gap-1 text-sm font-medium px-1 mb-1">
-                    Password <span className="text-red-500">*</span>
-                  </p>
-                  <div className="flex items-center border px-2 rounded-md">
-                    <Input
-                      type={showPassword ? "text" : "password"}
-                      placeholder="Enter your password"
-                      className="placeholder:font-poppins"
-                      value={password}
-                      onChange={(e) => setPassword(e.target.value)}
-                    />
-                    {showPassword ? (
-                      <FaRegEye
-                        onClick={() =>
-                          setShowPassword((showPassword) => !showPassword)
-                        }
+                  <div>
+                    <p className="flex gap-1 text-sm font-medium px-1 mb-1">
+                      Password <span className="text-red-500">*</span>
+                    </p>
+                    <div className="flex items-center border px-2 rounded-md">
+                      <Input
+                        required
+                        type={showPassword ? "text" : "password"}
+                        placeholder="Enter your password"
+                        className="placeholder:font-poppins"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
                       />
-                    ) : (
-                      <FaRegEyeSlash
-                        onClick={() =>
-                          setShowPassword((showPassword) => !showPassword)
-                        }
-                      />
-                    )}
+                      {showPassword ? (
+                        <FaRegEye
+                          onClick={() =>
+                            setShowPassword((showPassword) => !showPassword)
+                          }
+                        />
+                      ) : (
+                        <FaRegEyeSlash
+                          onClick={() =>
+                            setShowPassword((showPassword) => !showPassword)
+                          }
+                        />
+                      )}
+                    </div>
                   </div>
-                </div>
-              </CardContent>
-              <CardFooter className="mt-4">
-                <Button className="w-[80%] mx-auto" onClick={submitHandler}>
-                  Sign In
-                </Button>
-              </CardFooter>
-            </Card>
+                </CardContent>
+                <CardFooter className="mt-4">
+                  <Button
+                    type="submit"
+                    className="w-[80%] mx-auto"
+                    onClick={submitHandler}
+                  >
+                    Sign In
+                  </Button>
+                </CardFooter>
+              </Card>
+            </form>
           ) : (
-            <Card className="flex flex-col gap-2 ">
-              <CardHeader>
-                <CardTitle className="font-poppins text-3xl font-semibold">
-                  Sign Up
-                  <p className="font-normal text-sm mt-2">
-                    Already have an account ?{" "}
-                    <button
-                      className="text-blue-700 font-medium"
-                      onClick={() => setActiveForm("sign-in")}
-                    >
-                      Sign In
-                    </button>
-                  </p>
-                </CardTitle>
-              </CardHeader>
-              <CardContent className="flex flex-col gap-4">
-                <div>
-                  <p className="flex gap-1 text-sm font-medium px-1 mb-1">
-                    Email <span className="text-red-500">*</span>
-                  </p>
-                  <div className="flex items-center border px-2 rounded-md">
-                    <Input
-                      type="email"
-                      placeholder="Enter your email"
-                      className="placeholder:font-poppins"
-                      value={email}
-                      onChange={(e) => setEmail(e.target.value)}
-                    />
+            <form>
+              <Card className="flex flex-col gap-2 ">
+                <CardHeader>
+                  <CardTitle className="font-poppins text-3xl font-semibold">
+                    Sign Up
+                    <p className="font-normal text-sm mt-2">
+                      Already have an account ?{" "}
+                      <button
+                        className="text-blue-700 font-medium"
+                        onClick={() => setActiveForm("sign-in")}
+                      >
+                        Sign In
+                      </button>
+                    </p>
+                  </CardTitle>
+                </CardHeader>
+                <CardContent className="flex flex-col gap-4">
+                  <div>
+                    <p className="flex gap-1 text-sm font-medium px-1 mb-1">
+                      Email <span className="text-red-500">*</span>
+                    </p>
+                    <div className="flex items-center border px-2 rounded-md">
+                      <Input
+                        required
+                        type="email"
+                        placeholder="Enter your email"
+                        className="placeholder:font-poppins"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                      />
+                    </div>
                   </div>
-                </div>
 
-                <div>
-                  <p className="flex gap-1 text-sm font-medium px-1 mb-1">
-                    Password <span className="text-red-500">*</span>
-                  </p>
-                  <div className="flex items-center border px-2 rounded-md">
-                    <Input
-                      type={showPassword ? "text" : "password"}
-                      placeholder="Enter your password"
-                      className="placeholder:font-poppins"
-                      value={password}
-                      onChange={(e) => setPassword(e.target.value)}
-                    />
-                    {showPassword ? (
-                      <FaRegEye
-                        onClick={() =>
-                          setShowPassword((showPassword) => !showPassword)
-                        }
+                  <div>
+                    <p className="flex gap-1 text-sm font-medium px-1 mb-1">
+                      Password <span className="text-red-500">*</span>
+                    </p>
+                    <div className="flex items-center border px-2 rounded-md">
+                      <Input
+                        required
+                        type={showPassword ? "text" : "password"}
+                        placeholder="Enter your password"
+                        className="placeholder:font-poppins"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
                       />
-                    ) : (
-                      <FaRegEyeSlash
-                        onClick={() =>
-                          setShowPassword((showPassword) => !showPassword)
-                        }
-                      />
-                    )}
+                      {showPassword ? (
+                        <FaRegEye
+                          onClick={() =>
+                            setShowPassword((showPassword) => !showPassword)
+                          }
+                        />
+                      ) : (
+                        <FaRegEyeSlash
+                          onClick={() =>
+                            setShowPassword((showPassword) => !showPassword)
+                          }
+                        />
+                      )}
+                    </div>
                   </div>
-                </div>
 
-                <div>
-                  <p className="flex gap-1 text-sm font-medium px-1 mb-1">
-                    Confirm Password <span className="text-red-500">*</span>
-                  </p>
-                  <div className="flex items-center border px-2 rounded-md">
-                    <Input
-                      type={showPassword ? "text" : "password"}
-                      placeholder="Confirm password"
-                      className="placeholder:font-poppins"
-                      value={confirmPassword}
-                      onChange={(e) => setConfirmPassword(e.target.value)}
-                    />
-                    {showPassword ? (
-                      <FaRegEye
-                        onClick={() =>
-                          setShowPassword((showPassword) => !showPassword)
-                        }
+                  <div>
+                    <p className="flex gap-1 text-sm font-medium px-1 mb-1">
+                      Confirm Password <span className="text-red-500">*</span>
+                    </p>
+                    <div className="flex items-center border px-2 rounded-md">
+                      <Input
+                        required
+                        type={showPassword ? "text" : "password"}
+                        placeholder="Confirm password"
+                        className="placeholder:font-poppins"
+                        value={confirmPassword}
+                        onChange={(e) => setConfirmPassword(e.target.value)}
                       />
-                    ) : (
-                      <FaRegEyeSlash
-                        onClick={() =>
-                          setShowPassword((showPassword) => !showPassword)
-                        }
-                      />
-                    )}
+                      {showPassword ? (
+                        <FaRegEye
+                          onClick={() =>
+                            setShowPassword((showPassword) => !showPassword)
+                          }
+                        />
+                      ) : (
+                        <FaRegEyeSlash
+                          onClick={() =>
+                            setShowPassword((showPassword) => !showPassword)
+                          }
+                        />
+                      )}
+                    </div>
                   </div>
-                </div>
 
-                <div>
-                  <p className="flex gap-1 text-sm font-medium px-1 mb-1">
-                    Profile picture
-                  </p>
-                  <div className="flex items-center border px-2 rounded-md">
-                    <Input type="file" className="placeholder:font-poppins" />
+                  <div>
+                    <p className="flex gap-1 text-sm font-medium px-1 mb-1">
+                      Profile picture
+                    </p>
+                    <div className="flex items-center border px-2 rounded-md">
+                      <Input type="file" className="placeholder:font-poppins" />
+                    </div>
                   </div>
-                </div>
-              </CardContent>
-              <CardFooter className="mt-4">
-                <Button className="w-[80%] mx-auto" onClick={submitHandler}>
-                  {activeForm === "sign-in" ? "Sign In" : "Sign Up"}
-                </Button>
-              </CardFooter>
-            </Card>
+                </CardContent>
+                <CardFooter className="mt-4">
+                  <Button
+                    type="submit"
+                    className="w-[80%] mx-auto"
+                    onClick={submitHandler}
+                  >
+                    {activeForm === "sign-in" ? "Sign In" : "Sign Up"}
+                  </Button>
+                </CardFooter>
+              </Card>
+            </form>
           )}
         </div>
       </div>
