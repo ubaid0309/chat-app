f8839d650c6d469748f391e55dc4feaf6cffa900
ubaid0309 <ubaidskms@gmail.com>
2024-01-22 21:13:49 +0530

    add : connection to database

---
+++ backend/mongoose/connectToDatabase.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+require("dotenv").config();
+
+const MONGODB_URI = process.env.REACT_APP_MONGO_DB_URI;
+
+const connectToDatabase = async () => {
+  try {
+    const mongodbConnection = await mongoose.connect(MONGODB_URI);
+    console.log("Connected to MongoDB");
+  } catch (e) {
+    console.log("Failed to connect to MongoDB database: " + e.message);
+  }
+};
+
+module.exports = connectToDatabase;
--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,30 @@
 const express = require("express");
+
+
 const app = express();
+
 const chats = require("./data/data");
-require('dotenv').config()
-const cors = require('cors');
 
-const PORT = process.env.REACT_APP_PORT || 5000
-app.use(cors({
-    origin: 'http://localhost:5173'
-}))
-app.get('/', (req, res) => {
-    res.send("Api is running ")
-})
+require("dotenv").config();
 
-app.get('/api/chat', (req, res) => {
-    res.send(chats)
-})
+const cors = require("cors");
+const connectToDatabase = require("./mongoose/connectToDatabase");
 
+connectToDatabase();
 
-app.listen(PORT, console.log(`listening on port  ${PORT}`))
+const PORT = process.env.REACT_APP_PORT || 5000;
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
+app.get("/", (req, res) => {
+  res.send("Api is running ");
+});
+
+app.get("/api/chat", (req, res) => {
+  res.send(chats);
+});
+
+app.listen(PORT, console.log(`listening on port  ${PORT}`));
